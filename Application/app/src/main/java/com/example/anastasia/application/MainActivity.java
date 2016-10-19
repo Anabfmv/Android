568b1c6503f0ebc854cf34f48cb249c64c85d0a1
Anabfmv <anastasia.yarashukbfmv@gmail.com>
2016-10-19 11:23:56 +0300

    planning and menu

--- Application/app/src/main/java/com/example/anastasia/application/MainActivity.java
+++ Application/app/src/main/java/com/example/anastasia/application/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.anastasia.application;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymeny, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_settings:
+                Intent intent = new Intent(this,SettingsPage.class);
+                startActivity(intent);
+                break;
+            case R.id.menu_exit:
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.menu_settings:
+                Log.d("12345","click");
+                break;
+        }
     }
 }
