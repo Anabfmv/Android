d947584a4195926169fb62b4fb6127e20cf37e51
Anabfmv <anastasia.yarashukbfmv@gmail.com>
2016-11-14 11:02:43 +0300

    камера работает

--- Application/app/src/main/java/com/example/anastasia/application/FragmentAvatarStatus.java
+++ Application/app/src/main/java/com/example/anastasia/application/FragmentAvatarStatus.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,59 +20,22 @@ import java.io.File;
 /**
  * Created by Admin on 22.10.2016.
  */
-public class FragmentAvatarStatus extends Fragment implements View.OnClickListener {
+public class FragmentAvatarStatus extends Fragment{
     public ImageView ivPhoto;
     private String status;
-    File directory;
-    public static final int REQUEST_CODE_PHOTO = 1;
-    public static final int REQUEST_CODE_GALERY = 2;
+    public Button make_photo_btn;
+    public Button load_photo_bth;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.profile_avatar_settings_fragment, null);
-        ((TextView)view.findViewById(R.id.profile_status_string)).setText(CurrentUserInfo.getInstance(getActivity()).status);
-        createDirectory();
+        TextView status_bar = (TextView)view.findViewById(R.id.profile_status_string);
+        status_bar.setText(CurrentUserInfo.getInstance(getActivity()).status);
         ivPhoto = (ImageView) view.findViewById(R.id.settings_fragment_photo);
+        make_photo_btn = (Button)view.findViewById(R.id.Settings_fragent_make_photo);
+        load_photo_bth = (Button)view.findViewById(R.id.Settings_fragent_load_photo);
         return view;
     }
-
-
-    @Override
-    public void onClick(View view) {
-        Intent intent;
-        switch (view.getId()) {
-            case R.id.Settings_fragent_make_photo:
-                intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, generateFileUri());
-                startActivityForResult(intent, REQUEST_CODE_PHOTO);
-                break;
-            case R.id.Settings_fragent_load_photo:
-                intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent, REQUEST_CODE_GALERY);
-
-                break;
-        }
-    }
-
-    private Uri generateFileUri() {
-        File file = null;
-
-                file = new File(directory.getPath() + "/" + "photo_"
-                        + System.currentTimeMillis() + ".jpg");
-
-               return Uri.fromFile(file);
-    }
-
-    private void createDirectory() {
-        directory = new File(
-                Environment
-                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-                "MyFolder");
-        if (!directory.exists())
-            directory.mkdirs();
-    }
-
-
 }
--- Application/app/src/main/java/com/example/anastasia/application/ProfilePage.java
+++ Application/app/src/main/java/com/example/anastasia/application/ProfilePage.java
@@ -5,54 +5,100 @@ import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class ProfilePage extends AppCompatActivity {
 
+public class ProfilePage extends AppCompatActivity{
+
+    public static final int REQUEST_CODE_PHOTO = 1;
+    public static final int REQUEST_CODE_GALERY = 2;
+    File directory;
     FragmentAvatarStatus fragment_avater;
     Fragment fragment_name;
     FragmentTransaction fTrans;
+    CurrentUserInfo user;
+    private void setButtonsHandlers()
+    {
+        fragment_avater.load_photo_bth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_CODE_GALERY);
+            }
+        });
+        fragment_avater.make_photo_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //intent.putExtra(MediaStore.EXTRA_OUTPUT, getCorrentUserAvatarUri());
+                startActivityForResult(intent, REQUEST_CODE_PHOTO);
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //createDirectory();
         setContentView(R.layout.activity_profile_page);
         fragment_avater=new FragmentAvatarStatus();
-       ImageView v = fragment_avater.ivPhoto;
+        user=CurrentUserInfo.getInstance(this);
+        ImageView v = fragment_avater.ivPhoto;
         fragment_name=new FragmentNameEmail();
         fTrans = getFragmentManager().beginTransaction();
         fTrans.add(R.id.frame1,fragment_avater);
         fTrans.add(R.id.frame2,fragment_name);
         fTrans.commit();
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setButtonsHandlers();
+    }
+
+   /* private void createDirectory() {
+        directory = new File(getApplicationInfo().dataDir+"/Avatars");
+        if (!directory.exists())
+            directory.mkdirs();
+    }
+
+    private Uri getCorrentUserAvatarUri() {
+        File file = new File(getApplicationInfo().dataDir+"/Avatars" + "/" + "avatar_" + user.curent_id + ".jpg");
+        return Uri.fromFile(file);
+    }*/
+
     protected void onActivityResult(int requestCode, int resultCode,
                                     Intent intent)
     {
         switch (requestCode)
         {
-            case FragmentAvatarStatus.REQUEST_CODE_PHOTO:
+            case REQUEST_CODE_PHOTO:
                 if (resultCode == RESULT_OK)
                 {
-                    if (intent == null) {
-
-                    } else {
-                        Bundle bndl = intent.getExtras();
-                        if (bndl != null) {
-                            Object obj = intent.getExtras().get("data");
-                            if (obj instanceof Bitmap) {
-                                Bitmap bitmap = (Bitmap) obj;
-                                fragment_avater.ivPhoto.setImageBitmap(bitmap);
-                            }
+                   // String path = getCorrentUserAvatarUri().toString();
+                    //File file = new File(path);
+                    Bundle bndl = intent.getExtras();
+                    if (bndl != null) {
+                        Object obj = intent.getExtras().get("data");
+                        if (obj instanceof Bitmap) {
+                            Bitmap bitmap = (Bitmap) obj;
+                            fragment_avater.ivPhoto.setImageBitmap(bitmap);
                         }
                     }
                 }
                 break;
-            case FragmentAvatarStatus.REQUEST_CODE_GALERY:
+            case REQUEST_CODE_GALERY:
                 Bitmap img = null;
                 if (resultCode == RESULT_OK)
                 {
